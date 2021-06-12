a68c0382b7e506be22c9525dc0769a2b5c3cd8ea
jeremy543 <jeremy980128@gmail.com>
2021-06-11 23:21:11 -0400

    added two passed test

--- lab04/fizzbuzz_java/src/fizzbuzz/FizzbuzzTest.java
+++ lab04/fizzbuzz_java/src/fizzbuzz/FizzbuzzTest.java
@@ -14,5 +14,15 @@ class FizzbuzzTest {
 	void Fizzbuzz_test() {
 		assertEquals("9", obj.fizzbuzz(9));
 	}
+	@Test
+	void Fizzbuzz_test1() {
+		assertEquals("fizz", obj.fizzbuzz(5));
+	}
+	@Test
+	void Fizzbuzz_test2() {
+		assertEquals("buzz", obj.fizzbuzz(7));
+	}
+
+
 
 }
